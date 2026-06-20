624b102a24b6f2baa3033efbc7ae4f45052150f6
Kiselev Danila <kdanila03062006@gmail.com>
2026-06-20 22:23:33 +0300

    tests - 1

---
+++ __tests__/health.test.js
@@ -0,0 +1,21 @@
+import healthStatus from '../src/health';
+
+test('health > 50 should return healthy', () => {
+  expect(healthStatus({ name: 'Маг', health: 90 })).toBe('healthy');
+});
+
+test('health = 50 should return wounded', () => {
+  expect(healthStatus({ name: 'Маг', health: 50 })).toBe('wounded');
+});
+
+test('health between 15 and 50 should return wounded', () => {
+  expect(healthStatus({ name: 'Маг', health: 30 })).toBe('wounded');
+});
+
+test('health = 15 should return wounded', () => {
+  expect(healthStatus({ name: 'Маг', health: 15 })).toBe('wounded');
+});
+
+test('health < 15 should return critical', () => {
+  expect(healthStatus({ name: 'Маг', health: 10 })).toBe('critical');
+});
